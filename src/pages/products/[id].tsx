4b6307a64d314b3eebbc2c14d06bf065975fcefc
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-09-09 08:20:06 -0300

    add

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,5 +1,19 @@
+import Breadcrumb from "@/components/Breadcrumb";
+import PageWrapper from "@/components/PageWrapper";
+import { useRouter } from "next/router";
+
 export default function ProductPage(){
+    const router = useRouter();
+    const { id } = router.query;
+
     return (
-        <p>Detalhes do produto</p>
+        <PageWrapper>
+            <Breadcrumb 
+                items={[
+                    {title: "Smartphones"},
+                    {title: "Iphone 15 Pro Max"}
+                ]}
+            />
+        </PageWrapper>
     )
 }
